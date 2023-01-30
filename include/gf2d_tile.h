9efd64b4aff3a32315f9c0ec241d6e4880aa8aa8
AJ Fontaine <fontbane@gmail.com>
2023-01-30 16:11:03 -0500

    Initial commit with tile and map structs, build

---
+++ include/gf2d_tile.h
@@ -0,0 +1,59 @@
+#ifndef __GF2D_TILE_H__
+#define __GF2D_TILE_H__
+
+#include "gf2d_draw.h"
+#include "gf2d_graphics.h"
+#include "gf2d_sprite.h"
+
+/*!!Do not increase the following values!!*/
+#define MAX_TILES_IN_SHEET	SDL_MAX_UINT16
+#define MAX_TILES_IN_ROW	SDL_MAX_UINT8
+#define MAX_METATILES		SDL_MAX_UINT8
+/*!!Do not increase the above values!!*/
+
+#define TILE_SIZE 16
+
+typedef struct pointu8_s {
+	Uint8 x;
+	Uint8 y;
+}PointU8;
+
+/*The data stored for each tile in a tileset.*/
+typedef struct tile_s {
+	Uint16 tileID;		/*Frame of the tileset containing the tile*/
+	Uint8 metatile_id;	/*The id of the metatile with its behavior. 0 is normal.*/
+	Uint8 frames;		/*Number of extra frames of animation this tile has. 0 for none.*/
+}TileMetadata;
+
+/*An instance of a metatile. Can store a state and which frame of animation it's in.*/
+typedef struct metatile_instance_s {
+	TileMetadata* tileInfo;
+	Uint32 state;/*timers etc*/
+	float frame;/*for animated tiles*/
+}MetatileInstance;
+
+/*Config data for each tile in a tilemap. When you draw a map in an editor, each tile is this.*/
+typedef struct tilemap_tile_s {
+	PointU8 position;
+	Uint16 tileID;
+	Uint8 collisionType : 3;	/*User can have up to 8 elevations or collision types*/
+	Uint8 above_player : 1;		/*If tile renders above player and other entities*/
+	Uint8 pass_from_west : 1;
+	Uint8 pass_from_east : 1;
+	Uint8 pass_from_north : 1;
+	Uint8 pass_from_south : 1;
+	Uint8 flip_v : 1;
+	Uint8 flip_h : 1;
+}TilemapTile;
+
+#define CHUNK_SIZE	16	//The height and width of a chunk of tiles.
+
+/*An active tilemap tile. Up to 4 chunks of these can be stored, the rest are flushed.*/
+/*A TilemapTileActive will NOT be flushed if its metatile has had its state changed.*/
+/*Metatiles will only stay if their state has changed*/
+typedef struct active_tilemap_tile_s {
+	TilemapTile* configData;
+	MetatileInstance* metatile;
+}TilemapTileActive;
+
+#endif
